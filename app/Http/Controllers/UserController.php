f02ad19442e65281bad3bfc223fd8a55cdbdfb12
unknown <mhd.yari021@gmail.com>
2022-02-25 08:07:00 -0500

    Add update method user data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,7 @@ class UserController extends ApiController
         $request->validate([
             'first_name' => 'required',
             'last_name'  => 'required',
-            'email'      => 'email:rfc|unique:App\Models\User,email',
+            'email'      => 'email:rfc|unique:users,email',
             'password'   => 'required|min:6|confirmed'
         ]);
 
@@ -67,7 +67,39 @@ class UserController extends ApiController
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        // Validate user data
+        $request->validate([
+            'email'      => 'email:rfc|unique:users,email,' . $user->id,
+            'password'   => 'min:6|confirmed'
+        ]);
+
+        // Check data is change or not
+        if ($request->has('first_name')) {
+            $user->first_name = $request->first_name;
+        }
+
+        if ($request->has('last_name')) {
+            $user->last_name = $request->last_name;
+        }
+
+        if ($request->has('emaill') && $user->email != $request->email) {
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if (!$user->isDirty()) {
+            return $this->errorResponse("You need to specify a diffrent value to update", 422);
+        }
+
+        // Update user data
+        $user->save();
+
+        return $this->showOne($user);
     }
 
     /**
