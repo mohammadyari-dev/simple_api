bbfd4d24a27cc59227dd82ba40de9212d9577b7d
unknown <mhd.yari021@gmail.com>
2022-02-25 08:11:44 -0500

    Add delete method user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -108,8 +108,10 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return $this->showOne($user);
     }
 }
