8123f1b4efec736d5f69709953e497b7a62f5259
unknown <mhd.yari021@gmail.com>
2022-02-27 16:47:20 -0500

    Add UsersCategoryController

---
+++ app/Http/Controllers/User/UserCategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\ApiControllergot;
+use Illuminate\Http\Request;
+
+class UserCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+}
